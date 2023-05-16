60c9dc702b8226bb61b8a5eb8782dfdc1957bee1
dlin248 <122913408+dlin248@users.noreply.github.com>
2023-05-16 09:53:55 -0700

    Add files via upload

---
+++ Snake.java
@@ -0,0 +1,21 @@
+/**
+ *	The Snake is a singly linked list with snake behaviors.
+ *	@author	Darren lin 
+ *	@since	5 - 14 - 2023
+ */
+public class Snake extends SinglyLinkedList {
+	
+	// Fields
+	ListNode<Coordinate> head;
+	ListNode<Coordinate> tail;
+	public Snake(Coordinate location) 
+	{
+		 head = new ListNode<E>(location,
+		 for(int i = 0; i < 4; i++)
+		 {
+			 
+		 }
+	}
+	
+	
+}
